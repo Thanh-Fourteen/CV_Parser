64ae19d9af3a8d99a249ae816157dd6cf5f3f896
ngocminh173 <nnm010703@gmail.com>
2025-03-06 10:55:29 +0700

    add parameter

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import os.path as osp
 import tensorflow as tf
 print(tf.config.list_physical_devices())
